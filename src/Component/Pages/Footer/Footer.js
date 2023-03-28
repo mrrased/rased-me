8e8d8e87e062e5f9cc88acda3e6b739aafe3310c
mrrased <rashedtechdr.786@gmail.com>
2023-03-28 13:37:26 +0600

    dd

--- src/Component/Pages/Footer/Footer.js
+++ src/Component/Pages/Footer/Footer.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Footer = () => {
     return (
         <div style={{textAlign: 'center', fontSize: '16px', color: '#A9ADB8', fontWeight: 400, backgroundColor: '#0a101e', marginTop: '0px', paddingBottom: '12px'}}>
-            <p style={{marginTop: '0px'}}><span>©</span> 2020. Designed by Laralink. All right reserved.</p>
+            <p style={{marginTop: '0px'}}><span>©</span> 2024. Designed by Rashedur Rahman. All right reserved.</p>
         </div>
     );
 };
